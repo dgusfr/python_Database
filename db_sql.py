4f5ec34d90b5a898452a3d903fb28573d6509d88
dgusfr <diegogustavo.fr@gmail.com>
2025-12-02 11:29:26 -0300

    feat(define): disciplinas

--- db_sql.py
+++ db_sql.py
@@ -8,10 +8,11 @@ cursor = com.cursor()
 # Executa comandos SQL para criar uma tabela (se não existir)
 cursor.execute(
     """
-    CREATE TABLE IF NOT EXISTS estudantes (
+    CREATE TABLE IF NOT EXISTS disciplinas (
         id INTEGER PRIMARY KEY,
-        nome TEXT,
-        idade INTEGER
+        nome_disciplina TEXT,
+        estudante_id INTEGER
+        FOREIGN KEY (estudante_id) REFERENCES estudantes(id)
     )
 """
 )
